cc9167d5ef931a6d1c6ce9cab21c749743206621
Yash <yashmm2020@gmail.com>
2020-08-27 22:20:28 +0530

    registartion page linked and setings/database updated

--- Alumni_Portal/settings.py
+++ Alumni_Portal/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Alumni_Portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aap',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
