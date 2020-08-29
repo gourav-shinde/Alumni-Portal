de1c30cbe86a5a1da0598c11f77af910b6b98b4b
Yash <yashmm2020@gmail.com>
2020-08-29 16:49:43 +0530

    register and login completed

--- home/urls.py
+++ home/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('register1/', views.register1, name='register1'),
     path('register2/', views.register2, name='register2'),
-    path('register3/', views.register3, name='register3'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_validate, name='login'),
 
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth import authenticate, login, logout
+from .models import Student
+
 
 
 def home(request):
@@ -10,16 +14,67 @@ def register(request):
 
 
 def register1(request):
-    return render(request, 'home/alumniregister.html')
+    if request.method == "POST":
+        f_name = request.POST['first_name']
+        l_name = request.POST['last_name']
+        first_yr = request.POST['first_yr']
+        last_yr = request.POST['last_yr']
+        mobile = request.POST['mobile']
+        email = request.POST['email']
+        branch = request.POST['branch']
+        user_name = request.POST['user_name']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        user = User.objects.create_user(first_name=f_name, last_name=l_name, email=email, password=password,
+                                        username=user_name)
+        student_user = Student(firstname=f_name, lastname=l_name, email=email, password=password,
+                               username=user_name, graduation_starting_year=first_yr, graduation_ending_year=last_yr,
+                               mobile_no=mobile, branch=branch)
+        user.save()
+        student_user.save()
+        print("Student Data Added in Database")
+        return render(request, 'home/registerfinal.html')
+    else:
+        return render(request, 'home/studentregister.html')
 
 
 def register2(request):
-    return render(request, 'home/studentregister.html')
+    if request.method == "POST":
+        f_name = request.POST['first_name']
+        l_name = request.POST['last_name']
+        first_yr = request.POST['first_yr']
+        last_yr = request.POST['last_yr']
+        mobile = request.POST['mobile']
+        email = request.POST['email']
+        branch = request.POST['branch']
+        user_name = request.POST['user_name']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        user = User.objects.create_user(first_name=f_name, last_name=l_name, email=email, password=password,
+                                        username=user_name)
+        alumini_user = Student(firstname=f_name, lastname=l_name, email=email, password=password,
+                               username=user_name, graduation_starting_year=first_yr, graduation_ending_year=last_yr,
+                               mobile_no=mobile, branch=branch)
+        user.save()
+        alumini_user.save()
+        print("Alumini Data Added in Database")
+        return render(request, 'home/registerfinal.html')
+
+    else:
+        return render(request, 'home/alumniregister.html')
 
 
-def register3(request):
-    return render(request, 'home/registerfinal.html')
-
-
-def login(request):
-    return render(request, 'home/login.html')
+def login_validate(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            print('User')
+            login(request,user)
+            return redirect('home/login.html')
+        else:
+            print('Not User')
+            return redirect('/')
+    else:
+        return render(request,'home/login.html')
