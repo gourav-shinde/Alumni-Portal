5f2bfe5d5a54e9549115c518c1cb3fe2268e8e51
Yash <yashmm2020@gmail.com>
2020-09-02 14:31:04 +0530

    small bugs fixed

--- home/views.py
+++ home/views.py
@@ -2,66 +2,56 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-
+from .decorators import unauthenticated_user
 from .forms import CreateUserForm
 
 def home(request):
     return render(request, 'home/home.html')
 
-
+@unauthenticated_user
 def register(request):
-    if request.user.is_authenticated:
-        return redirect('/')
     return render(request, 'home/register1.html')
 
 # social authentication setting
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "home/home.html"
 
-
+@unauthenticated_user
 def register1(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
-                return redirect('/login')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('/login')
 
-        context = {'form': form}
-        return render(request, 'home/studentregister.html', context)
+    context = {'form': form}
+    return render(request, 'home/studentregister.html', context)
 
-
+@unauthenticated_user
 def register2(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
-                return redirect('/')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('/')
 
-        context = {'form': form}
-        return render(request, 'home/alumniregister.html', context)
+    context = {'form': form}
+    return render(request, 'home/alumniregister.html', context)
 
-
+@unauthenticated_user
 def login_validate(request):
-    if request.user.is_authenticated:
-        return redirect('/')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -69,6 +59,7 @@ def login_validate(request):
         if user is not None:
             print('User')
             login(request, user)
+            user = User.objects.get(username=username)
             return redirect('/')
         else:
             print('Not User')
@@ -89,6 +80,7 @@ def contact(request):
 
 def test(request):
     return render(request, 'home/test.html')
+
 
 @login_required(login_url='/login')
 def profile(request):
